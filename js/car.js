34bcc8f292a4446e6ce5d232f37308b6e128bf56
Christfried <christfriedbalizou@gmail.com>
2015-12-21 17:23:58 +0100

    Change: inherit move logic via an helper

--- js/car.js
+++ js/car.js
@@ -1,125 +1,56 @@
-function Car(color, quota, x, y){
+function Car(c, q, x, y){
 
     if(!(this instanceof arguments.callee)){
-        return new Car(color, quota, x, y);
+        return new Car(c, q, x, y);
     }
 
-    var _this = this;
-    this.color = color;
-    this.quota = quota;
-    this.store = [];
-    this.position = {
-        x : x || 0,
-        y : y || 0
+    var _this = new CarHelper(this)
+        ,color = c
+        ,quota = q
+        ,store = []
+        ,position = {x: x || 0 , y: y || 0}
+        ,path = {}
+        ,way = [];
+
+
+    this.setAttribute = function(key, value){
+            if(!this.hasOwnProperty(key)){
+                this[key] = value;
+            }
     };
-    this.path = {};
 
-    return {
-        quota : function(){
-            return _this.quota;
-        },
-        position : function(){
-            return _this.position;
-        },
-        moveTo : function(x, y){
+    this.position = function (x, y) {
+        if(x && y)
+            position = {x: x, y: y};
+      return position;
+    };
 
-            var element = _this
-                .table.children[y]
-                .children[x];
+    this.store = function(){
+        return store;
+    };
 
-            if( !_this.path[x+','+y] ){
-                _this.path[x+','+y] = element.innerHTML;
-            }
+    this.quota = function(){
+        return quota;
+    };
 
-            if(element.textContent === 'w'){
-                var _x = _this.position.x
-                    ,_y = _this.position.y;
-                _this
-                    .table.children[_y]
-                    .children[_x].innerHTML = _this.path[_x+','+_y];
-            }else{
-                element.innerHTML = 'w'.fontcolor(_this.color);
-            }
+    this.store = function(){
+        return store;
+    };
 
-            _this.position.x = x;
-            _this.position.y = y;
+    this.color = function(){
+        return color;
+    };
 
-            return true;
-        },
-        move : function(){
-            var direction = this.direction();
-            if(direction){
-               return this.moveTo(direction.x, direction.y);
-            }
-            return false;
-        },
-        direction: function(){
-            var where = []
-                ,x  = _this.position.x
-                ,y  = _this.position.y
-                ,tr = undefined;
+    this.path = function(key, value){
+        if(key && value)
+            if(!path[key])
+                path[key] = value;
+      return path;
+    };
 
-            if(_this.mapData.N > (y + 1)) {
-                tr = _this.table.children[y + 1];
-                // move right
-                if (/[wrgby\*]/.test(tr.children[x].textContent)) {
-                    where.push({x: x, y: (y + 1)});
-                }
-            }
-
-            if((y - 1) > 0) {
-                tr = _this.table.children[y - 1];
-                // move left
-                if (/[wrgby\*]/.test(tr.children[x].textContent)) {
-                    where.push({x: x, y: (y - 1)});
-                }
-            }
-
-            if((x - 1) > 0) {
-                tr = _this.table.children[y];
-                // move up
-                if (/[wrgby\*]/.test(tr.children[x - 1].textContent)) {
-                    where.push({x: (x - 1), y: y});
-                }
-            }
-
-            if(_this.mapData.N > (x + 1)) {
-                // move down
-                if (/[wrgby\*]/.test(tr.children[x + 1].textContent)) {
-                    where.push({x: (x + 1), y: y});
-                }
-            }
-
-            function random(min, max){
-                return Math.floor(Math.random() * (max - min)) + min;
-            }
-
-            return where[random(0, where.length)] || undefined;
-        },
-        canMove : function(){
-            if(_this.position.y < (_this.mapData.N - 1)){
-                if(_this.position.x < (_this.mapData.M - 1)){
-                    return true;
-                }
-            }
-            return false;
-        },
-        drop : function(){
-            return _this.store.pop();
-        },
-        pick : function(box){
-            if (_this.store.length < _this.quota) {
-                return _this.store.push(box);
-            }
-        },
-        store : function(){
-            return _this.store;
-        },
-        color : function(){
-            return _this.color;
-        },
-        setAttribut : function(key, value){
-            _this[key] = value;
-        }
+    this.way = function(value){
+        if(value)
+            way.push(value);
+        return way;
     };
 }
