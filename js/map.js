bd38189b83c62e3ad5eb08e5da4c71a4919de734
Christfried <christfriedbalizou@gmail.com>
2015-12-24 13:59:45 +0100

    change: init car path

--- js/map.js
+++ js/map.js
@@ -75,6 +75,7 @@ function Map(str){
             var tr = _this.table.children[p.y];
             var td = tr.children[p.x];
             td.innerHTML = 'w'.fontcolor(car.color());
+            car.path(x + ',' + y, td.innerHTML);
         }
 
     }
