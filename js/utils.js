198193d56d82fd5ab058500f468e708c3b7ecc19
Christfried Balizou <christfriedbalizou@gmail.com>
2015-12-31 00:36:17 +0100

    add: closest selector feature

--- js/utils.js
+++ js/utils.js
@@ -4,4 +4,30 @@
         return [this.x, this.y].join();
     };
 
+    Element.prototype.closest = function (selector) {
+        var func = ""
+            ,el = this;
+
+        // find vendor prefix
+        ['matches', 'webkitMatchesSelector', 'mozMatchesSelector', 'msMatchesSelector', 'oMatchesSelector']
+            .some(function (fn) {
+                if ('function' === typeof document.body[fn]) {
+                    func = fn;
+                    return true;
+                }
+                return false;
+            });
+
+        // traverse parents
+        while (el) {
+            var parent = el.parentElement;
+            if (parent && parent[func](selector)) {
+                return parent;
+            }
+            el = parent;
+        }
+
+        return undefined;
+    }
+
 })(window, document);
